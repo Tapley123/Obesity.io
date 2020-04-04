5fed315ff1080dbf0146debe779e6ec4807655de
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2020-04-03 20:56:39 -0800

    Chicken Movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float angle = 1.5f;
+
+    float rotY;
+
+    public float walkSpeed = 6;
+    public float jumpForce = 220;
+    Vector3 moveAmount;
+    Vector3 smoothMoveVelocity;
+    float verticalLookRotation;
+    //Transform cameraTransform;
+
+    private Rigidbody myRb;
+    private Vector3 moveDirection;
+
+
+    void Start()
+    {
+        myRb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+            transform.Rotate(Vector3.up, -angle);
+        else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+            transform.Rotate(Vector3.up, angle);
+
+        float inputY = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveDir = new Vector3(0, 0, inputY).normalized;
+        Vector3 targetMoveAmount = moveDir * walkSpeed;
+        moveAmount = Vector3.SmoothDamp(moveAmount, targetMoveAmount, ref smoothMoveVelocity, .15f);
+
+        // Jump
+        if (Input.GetButtonDown("Jump"))
+        {
+                myRb.AddForce(transform.up * jumpForce);
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        // Apply movement to rigidbody
+        Vector3 localMove = transform.TransformDirection(moveAmount) * Time.fixedDeltaTime;
+        myRb.MovePosition(myRb.position + localMove);
+    }
+}
