1c7d2ab498caa878aa24f83e1b1c48f7ac5a97ab
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2020-04-15 16:28:22 +0100

    Fixed door sound bug on last nugget

--- Assets/Scripts/Manager/DoorOpener.cs
+++ Assets/Scripts/Manager/DoorOpener.cs
@@ -7,35 +7,46 @@ public class DoorOpener : MonoBehaviour
     public GameObject[] doors;
     public static bool door1 = false, door2 = false, door3 = false, door4 = false;
     public AudioClip doorSound;
-    
+    private GameController gc;
+
+    private void Start()
+    {
+        gc = GameObject.Find("Game Controller").GetComponent<GameController>();
+    }
+
     void Update()
     {
-        if(door1)
+        //stops the doors working when you collect the last nugget
+        if (UIManager.nuggetAmount <= gc.amountOfNuggsToWin - 1)
         {
-            doors[0].GetComponent<Animator>().SetTrigger("Open");
-            doors[0].GetComponent<AudioSource>().PlayOneShot(doorSound);
-            door1 = false;
-        }
+            if (door1)
+            {
 
-        if (door2)
-        {
-            doors[1].GetComponent<Animator>().SetTrigger("Open");
-            doors[1].GetComponent<AudioSource>().PlayOneShot(doorSound);
-            door2 = false;
-        }
+                doors[0].GetComponent<Animator>().SetTrigger("Open");
+                doors[0].GetComponent<AudioSource>().PlayOneShot(doorSound);
+                door1 = false;
+            }
 
-        if (door3)
-        {
-            doors[2].GetComponent<Animator>().SetTrigger("Open");
-            doors[2].GetComponent<AudioSource>().PlayOneShot(doorSound);
-            door3 = false;
-        }
+            if (door2)
+            {
+                doors[1].GetComponent<Animator>().SetTrigger("Open");
+                doors[1].GetComponent<AudioSource>().PlayOneShot(doorSound);
+                door2 = false;
+            }
 
-        if (door4)
-        {
-            doors[3].GetComponent<Animator>().SetTrigger("Open");
-            doors[3].GetComponent<AudioSource>().PlayOneShot(doorSound);
-            door4 = false;
+            if (door3)
+            {
+                doors[2].GetComponent<Animator>().SetTrigger("Open");
+                doors[2].GetComponent<AudioSource>().PlayOneShot(doorSound);
+                door3 = false;
+            }
+
+            if (door4)
+            {
+                doors[3].GetComponent<Animator>().SetTrigger("Open");
+                doors[3].GetComponent<AudioSource>().PlayOneShot(doorSound);
+                door4 = false;
+            }
         }
     }
 }
